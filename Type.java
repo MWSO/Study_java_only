b9ad1ec8cfd1129c68e9477e271f56157399a07d
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-03-31 14:45:00 +0000

    [fixed]newJava

--- Type.java
+++ Type.java
@@ -12,5 +12,14 @@ public class Type{
     String age = "26";
     int n = Integer.parseInt(age);
     System.out.println("私の年齢は今年で" + (n + 1) + "になります");
+
+    int r = new java.util.Random().nextInt(100);
+    System.out.println("乱数テストです、100点満点中・・・" + r + "点でした！");
+
+    System.out.println("君の名前は？");
+    String name = new java.util.Scanner(System.in).nextLine();
+    System.out.println("君の年齢は？");
+    int yourAge = new java.util.Scanner(System.in).nextInt();
+    System.out.println("君は" + yourAge + "歳の" + name + "さんという方なのですね！");
   }
 }
