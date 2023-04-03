bd96da2e34604afeccd5a492783f7112c8d77987
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-04-03 14:19:37 +0000

    [fixed]hairetu

--- Score.java
+++ Score.java
@@ -4,5 +4,10 @@ public class Score {
     scores = new int[5];
     int num = scores.length;
     System.out.println("要素の数：" + num);
+    scores[1] = 30;
+    System.out.println(scores[1]);
+
+    int[] test = new int[] {30, 50, 70, 90};
+    System.out.println(test[2]);
   }
 }
