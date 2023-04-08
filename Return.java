0108dfbba3f6cf03b631a52df2d88dbf9611bd0b
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-04-08 16:58:01 +0000

    [fixed]study_return

--- Return.java
+++ Return.java
@@ -2,11 +2,23 @@ public class Return {
   public static void main(String[] args) {
     int ans = add(6, 2);
     System.out.println("計算結果は," + ans);
+    int[] array = makeArray(5);
+    for (int i : array) {
+      System.out.println(i);
+    }
   }
 
   public static int add(int x, int y) {
     int plus = x + y;
     int zyou = (plus *= plus);
     return zyou;
+  }
+
+  public static int[] makeArray(int size) {
+    int[] newArray = new int[size];
+    for ( int i = 0; i < newArray.length; i++) {
+      newArray[i] = i;
+    }
+    return newArray;
   }
 }
