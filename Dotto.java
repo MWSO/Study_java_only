78f7422b3a659f87afba37b49c5145edbd7a2230
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-04-12 13:56:56 +0000

    [start]array_flg

---
+++ Dotto.java
@@ -0,0 +1,28 @@
+public class Dotto {
+  public static void main(String[] args) {
+    //配列定義
+    int[] array = new int[10];
+    for (int i = 0; i < array.length; i++) {
+      array[i] = (i * 10);
+    }
+
+    boolean flg = true;
+    //[0]～[4]までの偶数
+    System.out.print("偶数：");
+    for (int i = 0; i > 5; i++) {
+      if (flg) {
+        System.out.print(array[i]);
+        flg = false;
+      } else {
+        System.out.print("," + array[i]);
+      }
+    }
+
+    flg = true;
+    //[5]～[9]までの奇数
+    System.out.print("奇数");
+    for (int i = 5; i < array.length; i++) {
+
+    }
+  }
+}
