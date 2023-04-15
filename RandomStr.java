5262356a4e36d29531cdd771192f8d9779fe7df8
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-04-15 14:52:14 +0000

    [add]MakeRandom

--- RandomStr.java
+++ RandomStr.java
@@ -2,5 +2,25 @@ import java.nio.charset.*;
 import java.util.*;
 
 public class RandomStr {
+  public static String RandomString(int i) {
+    int[] array = new int[256];
+    String mystring;
+    StringBuffer thebuffer;
+    String RandomNum;
 
+    new Random().nextInt(array);
+
+    mystring = new String(array, Charset.forName("UTF-8"));
+
+    thebuffer = new StringBuffer();
+
+    RandomNum = mystring.replaceAll("[^A-z0-9]", "");
+
+    for (int m = 0; m < RandomNum.length(); m++) {
+      if (Character.isLetter(RandomNum.charAt(m)) && (i > 0) || Character.isDigit(RandomNum.charAt(m)) && (i > 0)) {
+        thebuffer.append(Random.charAt(m));
+        i--;
+      }
+    }
+  }
 }
