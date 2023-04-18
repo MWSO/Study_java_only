ac0e39f6b3cc0bc98dea9c8c783898fb2cfeeb61
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-04-18 05:03:18 +0000

    [add]javaDatetime

--- TestAPI.java
+++ TestAPI.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 public class TestAPI {
   public static void main(String[] args) {
     int[] heights = {170, 195, 160, 155, 175};
@@ -5,5 +7,8 @@ public class TestAPI {
     for (int height : heights) {
       System.out.print(height + " ");
     }
+
+    Date date = new Date();
+    System.out.println("\n" + "今日は、" + date + "です！");
   }
 }
