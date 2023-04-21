d8b0cbbaf2630f46d8c5ae577d0b4488d7c261c8
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-04-21 14:40:00 +0000

    [add]StudyAPI_Map

--- TestAPI.java
+++ TestAPI.java
@@ -15,5 +15,16 @@ public class TestAPI {
       Arrays.asList("apple", "orange", "peach", "melon"));
     System.out.println(list);
     System.out.println(list.size());
+
+    for(Map.Entry<String, String> entry : map.entrySet()) {
+      System.out.println(entry.getKey() + ":" + entry.getValue());
+    }
+  }
+
+  private static Map<String, String> map = new HashMap<>();
+  static {
+    map.put("item1", "apple");
+    map.put("item2", "orange");
+    map.put("item3", "melon");
   }
 }
