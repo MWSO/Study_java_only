c0607bec2b0f05b4f65d011b453f5be9a4fbf22c
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-04-23 08:53:43 +0000

    [start]GuessTheNumber

---
+++ Guess.java
@@ -0,0 +1,6 @@
+public class Guess {
+  public static void main(String[] args) {
+    int Random = new java.util.Random().nextInt(10);
+    int Guess = new java.util.Scanner(System.in).nextInt();
+  }
+}
