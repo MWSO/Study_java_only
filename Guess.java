2665bb0f56f1b8671c3a0756cb6cb3a41c8a416c
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-04-23 09:06:38 +0000

    [fixed]fiveChanceGuess

--- Guess.java
+++ Guess.java
@@ -2,15 +2,18 @@ public class Guess {
   public static void main(String[] args) {
     int Random = new java.util.Random().nextInt(10);
     System.out.println("0～9までの数字を当ててみて");
-    int Guess = new java.util.Scanner(System.in).nextInt();
-    int gap = Math.abs(Random - Guess);
 
-    if (gap == 0) {
-      System.out.println("ピッタリ賞！");
-    } else if (gap <= 2) {
-      System.out.println("惜しい！");
-    } else {
-      System.out.println("全然違うよ");
+    for (int i = 0; i < 5; i++){
+      int Guess = new java.util.Scanner(System.in).nextInt();
+      int gap = Math.abs(Random - Guess);
+
+      if (gap == 0) {
+        System.out.println("ピッタリ賞！");
+      } else if (gap <= 2) {
+        System.out.println("惜しい！");
+      } else {
+        System.out.println("全然違うよ");
+      }
     }
   }
 }
