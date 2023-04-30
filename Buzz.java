f184b5b33fbc1a0e4fb1ba9b47dcda6e223a0c0f
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-04-30 12:33:00 +0000

    [end]fizzbuzz

--- Buzz.java
+++ Buzz.java
@@ -4,6 +4,13 @@ public class Buzz {
 
     if ((i % 3) == 0 && (i % 5) == 0) {
       result = "FizzBuzz";
+    } else if ((i % 3) == 0) {
+      result = "Fizz";
+    } else if((i % 5) == 0) {
+      result = "Buzz";
+    } else {
+      result = Integer.toString(i);
     }
+    return result;
   }
 }
--- Fizz.java
+++ Fizz.java
@@ -1,5 +1,7 @@
 public class Fizz {
   public static void main(String[] args) {
-    
+    System.out.println("FizzBuzzゲーム！" + "\n数字を入れてね！");
+    int i = new java.util.Scanner(System.in).nextInt();
+    System.out.println(Buzz.Buzz(i));
   }
 }
